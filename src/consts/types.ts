261207a4ab5b68475ea1c270a4cc2a2eceb387e7
Remssi <Zjanne95@gmail.com>
2021-08-18 20:03:59 +0300

    Add example comments to RuleContents type

--- src/consts/types.ts
+++ src/consts/types.ts
@@ -1,5 +1,7 @@
 export interface RuleContents {
+  // example: {1: ["1. Game Concepts", "100. General", "101. ..."]}
   tableOfContents: { [key: string]: string[] };
+  // example: {101: [{ text: 101. The Magic Golden Rules, additionalTexts: [] }, { text: 101.1. ... }]}
   chaptersAndRules: {
     [key: string]: { text: string; additionalTexts: string[] }[];
   };
