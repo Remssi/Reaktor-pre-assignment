c46ad5f01efcb5699b23d225f69a85ead324a04b
Remssi <Zjanne95@gmail.com>
2021-08-18 14:13:10 +0300

    Add prettier config file, remove npm lock file, add few comments

--- src/effects/index.ts
+++ src/effects/index.ts
@@ -1,9 +1,11 @@
+// * uses proxy url declared in package.json, which works in development
+// TODO: add absolute url for production
 export const getRules = (): void => {
   fetch("/2021/downloads/MagicCompRules%2020210419.txt")
     .then(response => {
-      return response.text()
+      return response.text();
     })
     .then(data => {
-      console.log(data)
-    })
-}
+      console.log(data);
+    });
+};
